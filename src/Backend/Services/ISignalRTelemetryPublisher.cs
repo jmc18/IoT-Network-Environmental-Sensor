0de4f77992e4011709ff936f97b7d266ce20efee
Javier MC <javierm@maddevelopment.com>
2026-04-29 14:59:41 -0600

    Purge

--- src/Backend/Services/ISignalRTelemetryPublisher.cs
+++
@@ -1,8 +0,0 @@
-using Backend.Models;
-
-namespace Backend.Services;
-
-public interface ISignalRTelemetryPublisher
-{
-    Task PublishReadingAsync(TelemetryUpdatedPayload payload, CancellationToken cancellationToken);
-}
